7a2aea1b622be460ed5cab68b8e15602907b04e2
Sternbach-Software <60489781+Sternbach-Software@users.noreply.github.com>
2026-03-10 16:37:11 +0000

    Migrate build scripts to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.13.2'
+        classpath("com.android.tools.build:gradle:8.13.2")
 
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0"
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -23,6 +23,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
